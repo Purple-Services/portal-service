b1e5160b7fbe3d249d756e72b88d440af92b5018
James <jmborden@gmail.com>
2016-10-25 12:10:51 -0500

    vehicles endpoint added

--- src/portal/users.clj
+++ src/portal/users.clj
@@ -1,55 +1,11 @@
 (ns portal.users
-  (:require [clojure.spec :as s]))
+  (:require [common.db :as db]))
 
-(s/def ::type (s/and string?
-                     #{"native"
-                       "facebook"
-                       "google"}))
+(defn vehicles-of-user-id
+  "Return all vehicles which match user-id"
+  [user-id]
+  (db/!select (db/conn) "vehicles"
+              [:id :user_id :year :make :model :color :gas_type :only_top_tier
+               :license_plate]
+              {:user_id user-id}))
 
-(s/def ::id string?)
-
-(def error-messages
-  {::id   [{:pred "string?"
-            :message "User ID must be a string!"}
-           {:pred "contains?"
-            :message "User ID must not be blank!"}
-           ]
-   ::type [{:pred "contains?"
-            :message "User type must not be blank!"}
-           {:pred  "string?"
-            :message "Type of user must be a string!"}
-           {:pred (str #{"native" "facebook" "google"})
-            :message "Type of user must be 'native','facebook', or 'google'"}
-           ]})
-
-(defn generate-error-messages
-  "Given a m returned by s/explain-data and a error-map, 
-  generate a list of human readable error messages"
-  [m error-map]
-  (let [problems (:clojure.spec/problems m)]
-    (map (fn [problem]
-           (if-let [path (first (:path problem))]
-             (let [pred (:pred problem)
-                   pred-message  (first (filter #(= (str pred) (:pred %)) (path error-map)))
-                   message (:message pred-message)]
-               ;;message
-               ;;path
-               ;;pred
-               ;;pred-message
-               ;;(path error-map)
-               message
-               )
-             (let [pred (:pred problem)
-                   path (last pred)
-                   pred-message  (first (filter #(= (str (first pred)) (:pred %)) (path error-map)))
-                   message (:message pred-message)]
-               ;;message
-               ;;path
-               ;;pred
-               ;;pred-message
-               message
-               ;;(path error-map)
-               )))
-         problems)))
-
-(s/def ::user (s/keys :req [::type ::id]))
