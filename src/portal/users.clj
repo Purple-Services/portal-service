b5dd4314f61b48386ed6d52bd9065c7428d63f61
James <jmborden@gmail.com>
2016-11-01 11:02:29 -0500

    refactored vehicles table in portal_cljs.js

--- src/portal/users.clj
+++ src/portal/users.clj
@@ -6,6 +6,6 @@
   [user-id]
   (db/!select (db/conn) "vehicles"
               [:id :user_id :year :make :model :color :gas_type :only_top_tier
-               :license_plate]
+               :license_plate :active]
               {:user_id user-id}))
 
