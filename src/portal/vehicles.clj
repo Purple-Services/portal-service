0d328b7aa2c3ead503a55d00e97edaf2e3d0ed04
James <jmborden@gmail.com>
2016-11-01 12:56:37 -0500

    vehicles has its own namespace

--- src/portal/vehicles.clj
+++ src/portal/vehicles.clj
@@ -1,11 +1,10 @@
-(ns portal.users
+(ns portal.vehicles
   (:require [common.db :as db]))
 
-(defn vehicles-of-user-id
+(defn vehicles
   "Return all vehicles which match user-id"
   [user-id]
   (db/!select (db/conn) "vehicles"
               [:id :user_id :year :make :model :color :gas_type :only_top_tier
                :license_plate :active]
               {:user_id user-id}))
-
