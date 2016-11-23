866db8adf46a4392d7bd5b001672422634f26555
James <jmborden@gmail.com>
2016-11-23 15:59:54 -0600

    downgrading ring-jetty-adapater to 1.5.0 so it will work on travis ci

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                  [ring/ring-devel "1.5.0"]
                  [ring/ring-json "0.4.0"]
                  [ring/ring-mock "0.3.0"]
-                 [ring/ring-jetty-adapter "1.6.0-beta6"]
+                 [ring/ring-jetty-adapter "1.5.0"]
                  [ring-cors "0.1.8"]]
   :plugins [[lein-ring "0.9.7"]
             [lein-beanstalk "0.2.7"]
